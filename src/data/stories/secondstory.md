fa16e269fb08a8ea730dd5049e34174fb817ece7
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2019-11-26 18:08:33 -0500

    Work on filtering.
    Waiting to add md values in md files to allow filtering.

--- src/data/stories/secondstory.md
+++ src/data/stories/secondstory.md
@@ -1,8 +1,9 @@
 ---
 templateKey: story-post
-title: Jane Doe
-country: England
+title: Jane Doe2
+country: Ireland
 date: 2019-11-26T04:13:30.895Z
+image: ../../assets/demo.jpeg
 description: asdankb vasdnekfl dlqnlasd
 featuredpost: true
 featuredimage: /assets/uploads/logotrial2.png
--- src/pages/selection.jsx
+++ src/pages/selection.jsx
@@ -4,14 +4,21 @@
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useStaticQuery, graphql, navigate } from 'gatsby';
 import { Card, CardHeader, CardMedia, Button as MaterialButton, CardActions, Checkbox } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { withSeo } from '@utils';
 import { Page } from '@layouts';
+import { DynamicCheckbox } from '@components';
+// Drawer
 import { FormControl, Drawer, List, ListSubheader, ListItem, ListItemText, Collapse } from '@material-ui/core';
+// Drawer
 import { FormControlLabel, FormLabel, Radio, RadioGroup } from '@material-ui/core'
-import { CardActionArea, CardContent, Typography } from '@material-ui/core'
+// Cards
+import { CardActionArea, CardContent, Typography, Fab } from '@material-ui/core'
+import { NavigationIcon } from '@material-ui/icons'
+// Drawer 
 import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
 
@@ -24,7 +31,7 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(2),
   },
   list: {
-    width: 250,
+    width: 200,
   },
   fullList: {
     width: 'auto',
@@ -110,11 +117,14 @@ function SortNameMenu() {
   );
 } */
 
-function NestedList() {
+function FilterDrawer() {
+
   const nestedClasses = useStyles();
+
   const [nameOpen, setNameOpen] = React.useState(true);
   const [countryOpen, setCountryOpen] = React.useState(false);
   const [tagOpen, setTagOpen] = React.useState(false);  
+
   const [filterNameSetting, setNameSetting] = React.useState('A to Z');
   const [filterCountrySetting, setCountrySetting] = React.useState(['Include All']);
   const [filterTagSetting, setTagSetting] = React.useState([]); 
@@ -122,11 +132,9 @@ function NestedList() {
 
   // Possible Options
   const nameSorting = ['A to Z', 'Z to A']
-  const countries = ['Include All', 'England', 'Scotland', 'Wales', 'Ireland', 'Germany'] 
+  const countries = ['England', 'Scotland', 'Wales', 'Ireland', 'Germany'] 
   var tags = ['Tag1', 'Tag2', 'Tag3']
   var checkboxes = []; 
-
-  var boxChecked = false; 
 
   const handleClick = (e) => {
     if (e === 0) setNameOpen(!nameOpen);
@@ -145,21 +153,21 @@ function NestedList() {
 
   const handleCountryChange = event => {
     console.log(event.target.name + " " + event.target.checked);
-    boxChecked = !boxChecked;
 
-    if (!event.target.checked)
+    if (event.target.name === 'Include All')  {
+      setCountryOpen (!event.target.checked); 
+      if (event.target.checked) setCountrySetting(countries); 
+      else setCountrySetting([]); 
+    }
+    // if box is no longer selected
+    else if (!event.target.checked)
     {
       var index = filterCountrySetting.indexOf(event.target.name);
       if (index > -1) filterCountrySetting.splice(index, 1); 
     }
     else 
-    {
-      if (event.target.name === 'Include All')  {
-        setCountrySetting(['Include All']) 
-        checkboxes.map( item => { console.log(item.props.value) } ); 
-      }
-      else 
-        (filterCountrySetting[0] === 'Include All') ? setCountrySetting([event.target.name]) : filterCountrySetting.push(event.target.name);
+    { 
+      filterCountrySetting.push(event.target.name);
     }
     console.log(filterCountrySetting);
     updateFilter(); 
@@ -168,125 +176,25 @@ function NestedList() {
   const handleTagChange = event => {
     console.log(event.target.name + " " + event.target.checked);
 
-    if (!event.target.checked)
+    if (event.target.name === 'Include All')  {
+      setTagOpen (!event.target.checked); 
+      if (event.target.checked) setTagSetting(tags); 
+      else setTagSetting([]); 
+    }
+    // if box is no longer selected
+    else if (!event.target.checked)
     {
       var index = filterTagSetting.indexOf(event.target.name);
       if (index > -1) filterTagSetting.splice(index, 1); 
     }
     else 
-    {
+    { 
       filterTagSetting.push(event.target.name);
     }
     console.log(filterTagSetting);
     updateFilter(); 
   }
 
-  return (
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Filter Stories
-        </ListSubheader>
-      }
-      className={nestedClasses.root}
-    >
-      { /* Name Filtering */ }
-      <ListItem button onClick={handleClick.bind(this, 0)}>
-        <ListItemText primary="by Name" />
-        {nameOpen ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={nameOpen} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-
-        <FormControl component="fieldset" className={nestedClasses.formControl}>
-        <FormLabel component="legend"></FormLabel>
-        <RadioGroup aria-label="name" name="name1" value={filterNameSetting} onChange={handleNameChange}>
-
-          {nameSorting.map( item => {
-            return (
-            <ListItem button className={nestedClasses.nested}>
-              <FormControlLabel value={item} control={<Radio />} label={item} />
-            </ListItem>
-            )
-          })}
-          </RadioGroup>
-        </FormControl>
-
-
-        </List>
-      </Collapse> 
-
-      { /* Country Filtering */ }
-
-      <ListItem button onClick={handleClick.bind(this, 1)}>
-        <ListItemText primary="by Country" />
-        {countryOpen ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={countryOpen} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-
-        {countries.map( item => {return (
-          <ListItem button className={nestedClasses.nested}>
-            <Checkbox name={item} label={item} onClick={handleCountryChange}></Checkbox>{item}
-          </ListItem>) } ) } 
-
-        </List>
-      </Collapse>         
-
-
-
-      { /* Tag Filtering */ }
-
-      <ListItem button onClick={handleClick.bind(this, 2)}>
-        <ListItemText primary="by Tag" />
-        {tagOpen ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={tagOpen} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-
-        { tags.map(
-           item => 
-           <ListItem button className={nestedClasses.nested}>
-          <Checkbox name={item} label={item} onClick={handleTagChange} ></Checkbox>{item}
-           </ListItem>
-          )
-        }
-        </List>
-      </Collapse> 
-     </List>
-  );
-}
-{/*
-  TODO: Figure out how to change the checked/unchecked state of a button through code. 
-  WAS TRYING TO MAKE A CONTROLLABLE/DYNAMIC CHECK BOX HERE
-class DynamicCheckBox extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {value: false, chk = 2}
-  }
-
-  handleChange = event => {
-    console.log("changed" + this.state.value + " " + chk.checked);
-    this.setState({value: !this.state.value})
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  render() {
-    return chk = (
-      <CheckBox
-      onClick={this.handleChange} checked={this.state.value}>
-      </CheckBox> 
-    ); 
-  }
-}*/ }
-
-function FilterDrawer() {
   const [drawerState, setDrawerState] = React.useState({
     left: false,
   });
@@ -300,10 +208,94 @@ function FilterDrawer() {
   };
 
   const sideList = side => (
-    <div>
+    <div
+      className={nestedClasses.list}
+    >
       <List>
-        <NestedList>
-        </NestedList>
+        <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Filter Stories
+          </ListSubheader>
+        }
+        className={nestedClasses.root}
+      >
+        { /* Name Filtering */ }
+        <ListItem button onClick={handleClick.bind(this, 0)}>
+          <ListItemText primary="by Name" />
+          {nameOpen ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={nameOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+
+          <FormControl component="fieldset" className={nestedClasses.formControl}>
+            <FormLabel component="legend"></FormLabel>
+            <RadioGroup aria-label="name" name="name1" value={filterNameSetting} onChange={handleNameChange}>
+
+              {nameSorting.map( item => {
+                return (
+                <ListItem button className={nestedClasses.nested}>
+                  <FormControlLabel value={item} control={<Radio />} label={item} />
+                </ListItem>
+                )
+              })}
+              </RadioGroup>
+          </FormControl>
+
+
+          </List>
+        </Collapse> 
+
+        { /* Country Filtering */ }
+
+        <ListItem>
+          <ListItemText primary="by Country" />        
+        </ListItem>
+        <Collapse in={true} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+          <ListItem button className={nestedClasses.nested}>
+            <Checkbox name='Include All' label='Include All' onClick={handleCountryChange} defaultChecked={!countryOpen}></Checkbox>Include All 
+              </ListItem>
+          </List>
+        </Collapse>
+        <Collapse in={countryOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+
+          {countries.map( item => {return (
+            <ListItem button className={nestedClasses.nested}>
+              <Checkbox name={item} label={item} onClick={handleCountryChange}></Checkbox>{item}
+            </ListItem>) } ) } 
+
+          </List>
+        </Collapse>       
+
+        { /* Tag Filtering */ }
+
+        <ListItem>
+          <ListItemText primary="by Tag" />
+        </ListItem>
+        <Collapse in={true} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+          <ListItem button className={nestedClasses.nested}>
+            <Checkbox name='Include All' label='Include All' onClick={handleTagChange} defaultChecked={!tagOpen}></Checkbox>Include All 
+          </ListItem>
+          </List>
+        </Collapse>
+        <Collapse in={tagOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding> 
+
+          { tags.map(
+            item => 
+            <ListItem button className={nestedClasses.nested}>
+            <Checkbox name={item} label={item} onClick={handleTagChange} ></Checkbox>{item}
+            </ListItem>
+            )
+          }
+          </List>
+        </Collapse> 
+      </List>
       </List>
     </div>
   );
@@ -314,6 +306,14 @@ function FilterDrawer() {
       <Drawer open={drawerState.left} onClose={toggleDrawer('left', false)}>
         {sideList('left')}
       </Drawer>
+
+      <FilterResults
+        countryFilter={filterCountrySetting}
+        tagFilter={filterTagSetting}
+        includeAllCountries={!countryOpen}
+        includeAllTags={!tagOpen}
+        >
+      </FilterResults>
     </div>
   );
 }
@@ -336,19 +336,45 @@ const SelectionPage = () => (
         </FilterDrawer>
       </Grid>
 
-      <FilterResults>
-      </FilterResults>   
-
       </div>
     </Page>
 );
 
 // Displays results from filtering 
-function FilterResults()
+function FilterResults(props)
 {
-  const rootSettings = useStyles()
-  const storiesToDisplay = [ { title: 'story1', desc: 'first last 1', country: 'England'}, { title: 'story2', desc: 'first last 2', country: 'Ireland'} ]
+  const query = graphql`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___order, order: ASC }) {
+        edges {
+          node {
+            frontmatter {
+              title
+              image {
+                publicURL
+              }
+              description
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
 
+const { allMarkdownRemark } = useStaticQuery(query);
+  const tagStyles = makeStyles(theme => ({
+    margin: {
+      margin: theme.spacing(5),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }));
+
+  const rootSettings = useStyles()
 
   return (
     <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
@@ -359,15 +385,34 @@ function FilterResults()
         </ListSubheader>
     }
     className={rootSettings.root}>
-      {storiesToDisplay.map( story => { return(
-        <div>
-        <SelectionCard
-           title={story.title}
-           desc={story.desc}>
-        
-           </SelectionCard>
-        </div>
-      ) } )} 
+
+      {
+        allMarkdownRemark.edges.map(({ node }, i) => (
+          // <SelectionCard
+          //    title={node.frontmatter.title}
+          //    desc={node.frontmatter.description}
+          //    tags={['Tag1']}>
+
+          //    </SelectionCard> 
+           console.log(node.frontmatter.title + ' ' + node.frontmatter.country + ' ' + node.frontmatter.tags)
+         ))
+      } 
+      {
+
+        allMarkdownRemark.edges.filter(({ node }, i) => (
+          node.frontmatter.country != null && node.frontmatter.tags != null &&
+          (props.countryFilter.includes(node.frontmatter.country) || props.includeAllCountries) &&
+          (props.tagFilter.includes(node.frontmatter.tags) || props.includeAllTags))).map(({node}, i) => (
+           <SelectionCard
+             title={node.frontmatter.title}
+             desc={node.frontmatter.description}
+             tags={['Tag1']}>             
+             </SelectionCard>
+      
+         )) 
+
+      }
+
     </List>
     </div>
   )
@@ -383,7 +428,7 @@ function SelectionCard(props) {
         <CardMedia
           component="img"
           alt="Sample Story"
-          height="140"
+          height="225"
           image="/static/demo-f362adb686ed8fa86a8c5e96437a264c.jpeg"
           title="Sample Story"
         />
@@ -397,6 +442,20 @@ function SelectionCard(props) {
           <Typography variant="body2" color="textSecondary" component="p">
             Here is space for a little description to hook reader.
           </Typography>
+          
+          {
+            props.tags.map( tagText => 
+              <Fab
+                variant="extended"
+                size="small"
+                color="primary"
+                aria-label="add"
+              >
+                {tagText}
+              </Fab>
+            )  
+
+          }
         </CardContent>
       </CardActionArea>
       <CardActions>
