a3f7d412326e238a3902d2421c1e6ceadd533006
Chidubem Ogbudibe <chidubem@live.ca>
2020-01-03 18:36:56 -0700

    update page.jsx

--- src/layouts/Page.jsx
+++ src/layouts/Page.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Footer from '@components/Footer';
-import favicon from '@assets/logo-icon.png';
 import '@styles/layouts/Page.scss';
 import { Container } from '@material-ui/core';
 
 export const Page = ({ children, className, ...props }) => (
   // <main className={`page${className ? ` ${className}` : ''}`} {...props}>
   <div>
-    <Helmet>
-      <link rel="icon" href={favicon} />
-    </Helmet>
     <Container className='page__content'>{children}</Container>
     <Footer/>
   </div>
