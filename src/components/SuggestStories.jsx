3d6ff1396b1a28508c2973fba7a22c1931ad7777
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2020-01-10 14:52:54 -0500

    Blog page formatting

--- src/components/SuggestStories.jsx
+++ src/components/SuggestStories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import { Grid } from '@material-ui/core';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -7,11 +8,14 @@ import Typography from '@material-ui/core/Typography';
 import '@styles/components/SuggestStories.scss';
 
 export const SuggestStories = ({ edges }) => (
-    <div className='spacer'>
-    <h3>Recent Stories:</h3>
-    <div className='suggestContainer'>
-
-        <div className='suggestRow'>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <h3>Recent Stories</h3>
+    <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
             <Card onClick={()=>window.open(`${edges[edges.length-1].node.fields.slug}`, "_self")} className='suggestCard'>
                 <CardActionArea
                     to={edges[edges.length-1].node.fields.slug}
@@ -55,10 +59,6 @@ export const SuggestStories = ({ edges }) => (
                     </CardContent>
                 </CardActionArea>
             </Card>
-        </div>
-
-
-        <div className='suggestRow'>
             <Card onClick={()=>window.open(`${edges[edges.length-3].node.fields.slug}`, "_self")} className='suggestCard'>
                 <CardActionArea
                     to={edges[edges.length-3].node.fields.slug}
@@ -102,9 +102,7 @@ export const SuggestStories = ({ edges }) => (
                     </CardContent>
                 </CardActionArea>
             </Card>
-        </div>
-
-        </div>
+            </Grid>
     </div>
     
 );
